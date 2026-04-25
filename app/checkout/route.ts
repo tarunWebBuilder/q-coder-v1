c5c400135e6e20d8967126baeb53531c0a1c58c6
Tarun <tarunpahade55@gmail.com>
2026-04-25 18:06:40 +0530

    Removed pay and

--- app/checkout/route.ts
+++ app/checkout/route.ts
@@ -4,7 +4,6 @@ import { Checkout } from "@dodopayments/nextjs";
 export const GET = Checkout({
   bearerToken: process.env.DODO_PAYMENTS_API_KEY,
   returnUrl: process.env.DODO_PAYMENTS_RETURN_URL,
-  environment: process.env.DODO_PAYMENTS_ENVIRONMENT,
   type: "static", // optional, defaults to 'static'
 });
 
@@ -12,6 +11,5 @@ export const GET = Checkout({
 export const POST = Checkout({
   bearerToken: process.env.DODO_PAYMENTS_API_KEY,
   returnUrl: process.env.DODO_PAYMENTS_RETURN_URL,
-  environment: process.env.DODO_PAYMENTS_ENVIRONMENT,
   type: "session", // for checkout sessions
 });
